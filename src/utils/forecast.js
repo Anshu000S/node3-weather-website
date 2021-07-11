0abe8c20fcbab4e21aa3d169b63a21c71629c477
Anshuman Baruah <anshumanbaruah001@gmail.com>
2021-07-11 19:20:05 +0530

    New data is added in the weather information

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,4 +1,6 @@
 const request = require('request')
+
+
 const forecast = (latitude, longitude, callback) =>{
     const url ='http://api.weatherstack.com/current?access_key=86b9d47f8011d8a499740f4ddc7822b6&query='+longitude+','+latitude
 
@@ -8,7 +10,7 @@ const forecast = (latitude, longitude, callback) =>{
         } else if(body.error){
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0]+'. Currently it is '+body.current.temperature+' degrees outside. IT feels like '+body.current.feelslike+' degrees outside')
+            callback(undefined, body.current.weather_descriptions[0]+'. Currently it is '+body.current.temperature+' degrees outside. IT feels like '+body.current.feelslike+' degrees outside. The Humidity is '+body.current.humidity+'%.')
         }
     })
 }
